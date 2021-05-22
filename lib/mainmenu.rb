c81c96cc294d8264041c833714f0887f13ad57f0
wpham1 <gcab012112@coderacademy.edu.au>
2021-05-22 23:35:16 +1000

    added fancy new title w colors and added ascii art file

--- lib/mainmenu.rb
+++ lib/mainmenu.rb
@@ -1,14 +1,22 @@
-require_relative 'character'
 require "tty-prompt"
+require "artii"
+require "colorize"
 prompt = TTY::Prompt.new
-
 
 class Intro
     
     def logo
-    puts "COINNNNNNNNPUNCHHHHHHHHHHH"
+    ascii = Artii::Base.new :font => 'cosmic'
+    ascii2 = Artii::Base.new :font => 'cosmike'
+    puts ascii.asciify('                t e r m i n a l ').light_red
+    puts ascii2.asciify('             c o i n').yellow
     end
     
 end
 Intro.new.logo
-Character
+sleep(1)
+system "clear"
+sleep(1)
+require_relative "ascii_art.rb"
+Ascii_art.new.fire_place
+require_relative "character"
