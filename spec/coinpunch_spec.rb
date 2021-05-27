9687f8661a5f43dc5dc74de0a95727f6b8d964af
wpham1 <gcab012112@coderacademy.edu.au>
2021-05-27 10:33:02 +1000

    added more RSPEC definitions testing

--- spec/coinpunch_spec.rb
+++ spec/coinpunch_spec.rb
@@ -1,9 +1,36 @@
-require 'hub'  #=> add this
-require 'character'
+# require 'hub'  #=> add this
+# require 'character'
 
-Rspec.describe Shop do
-    it 'creates a student class' do
-      shop = Shop.new
-      expect(shop).to be_kind_of(Shop)
+RSpec.describe "an empty array for items" do
+  let(:inventory) {[]}
+  it "has no items" do
+    expect(inventory.size).to eq(0)
+  end
+  it 'can have an item added' do
+    inventory << "sword"
+    expect(inventory).to eq(["sword"])
+  end
+  it 'can have multiple items added' do
+    inventory << "sword"
+    inventory << "shield"
+    expect(inventory).to eq(["sword","shield"])
+  end
+end
+
+RSpec.describe 'Define get name method' do
+  def user_name(name)
+    "Hello #{name}"
+  end
+
+  it 'uses get_name method' do
+    user_input = user_name('Hero')
+    expect(user_input).to eq('Hello Hero')
+  end
+
+  context 'within a context group' do
+    it 'can still use get_name method' do
+      message = user_name('Villain')
+      expect(message).to eq('Hello Villain')
     end
   end
+end
